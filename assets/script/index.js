5f9b0f5789fd9453bdc926772ff46d2164e3ba3c
BrianMcL <bmclaugh28@yahoo.com>
2019-11-17 17:00:24 -0500

    experiment with fromCharCode

---
+++ assets/script/index.js
@@ -0,0 +1,8 @@
+/***********************************************/
+var pwd 
+
+pwd = String.fromCharCode (82); 
+pwd = pwd + String.fromCharCode (83);
+pwd = pwd + String.fromCharCode (84);
+window.alert (pwd);
+  
