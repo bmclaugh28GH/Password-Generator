187f8b68b495f848613d7181733409f4f0fc9414
BrianMcL <bmclaugh28@yahoo.com>
2019-11-19 14:48:20 -0500

    incorporated user input, added .png files and readMe.md text

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,18 +1,16 @@
+/***********************************************
+ * Globals 
 /***********************************************/
+
 var pwd = ""; 
 var pwdLength;
 var pwdMinLength = 8; 
 var pwdMaxLength = 128; 
-var upperCaseChar;
-var upperCasePos; 
-var lowerCaseChar;
-var lowerCasePos;
-var specChar;
-var specCharPos;
-var numChar;
-var numCharPos;
-var randomType; 
-var randomChar; 
+
+var upperCaseSelected = false; 
+var lowerCaseSelected = false; 
+var specCharSelected = false; 
+var numCharSelected = false; 
 
 /************************************************
  * get a random number within the given range 
@@ -22,121 +20,146 @@ function GetRandomNumInRange (myLowerBound, myUpperBound) {
 
     return Math.floor (Math.random () * (myUpperBound - myLowerBound + 1)) + myLowerBound;
 
-}
+} // GetRandomNumInRange
 
 /************************************************
- * convert a number to its ASCII character  
+ * convert an ASCII number to its character  
  * *********************************************/
 
 function GetChar (myNum) {
 
     return String.fromCharCode (myNum);
 
-}
+} // GetChar
+
+// **********************************************
+// Generate a password 
+// **********************************************
 
 function GeneratePwd () {
 
-    /************************************************
-     * randomize the length of the password 
-     ***********************************************/
-
     pwd = '';
-    pwdLength = GetRandomNumInRange (pwdMinLength, pwdMaxLength); 
     console.log ("pwd length will be " + pwdLength);
 
     /************************************************
-     * force there to be at least one each of upper case, lower case, 
-     * a special char and a number 
+     * Load up the character types the user wants to use. Up to 4.  
      ***********************************************/
 
-    upperCaseChar = GetChar (GetRandomNumInRange (65, 90)); 
-    upperCasePos = GetRandomNumInRange (1, pwdLength);
-    console.log ("upper case " + upperCaseChar + " pos " + upperCasePos);
-
-    /* no dups!! */
-    lowerCaseChar = GetChar (GetRandomNumInRange (97, 122));
-    lowerCasePos; 
-    do {
-
-        lowerCasePos = GetRandomNumInRange (1, pwdLength);
+    var charTypeList=[]; 
+    if (upperCaseSelected===true) {
+        charTypeList.push("U");
     }
-    while  (lowerCasePos === upperCasePos); 
-    console.log ("lower case " + lowerCaseChar + " pos " + lowerCasePos);
-
-    specChar = GetChar (GetRandomNumInRange (35, 47));
-    specCharPos; 
-    do {
-        specCharPos = GetRandomNumInRange (1, pwdLength);
-    } 
-    while (specCharPos === upperCaseChar || specCharPos === lowerCasePos); 
-    console.log ("upper case " + specChar + " pos " + specCharPos);
-
-    numChar = GetRandomNumInRange (0, 9); 
-    numCharPos; 
-    do {
-        numCharPos = GetRandomNumInRange (1, pwdLength);
+    if (lowerCaseSelected===true) {
+        charTypeList.push("L");
     }
-    while (numCharPos === upperCaseChar || numCharPos === lowerCasePos || numCharPos === specCharPos); 
-    console.log ("upper case " + numChar + " pos " + numCharPos);
+    if (specCharSelected===true) {
+        charTypeList.push("S");
+    }
+    if (numCharSelected===true) {
+        charTypeList.push("N");
+    }
+    console.log (charTypeList); 
 
     /************************************************
-     * Ive got 4 mandatory characters and a length. Now start building the pwd
-     * Use my preassigned values in their positions, otherwise get another random 
-     * value 
+     * Start building the pwd based on user criteria 
      ***********************************************/
 
     for (i=1; i<=pwdLength; i++) {
 
-        switch (i) {
-        case upperCasePos: 
-            pwd = pwd + upperCaseChar;
-            break;
-        case lowerCasePos:
-            pwd = pwd + lowerCaseChar;    
-            break;
-        case specCharPos: 
-            pwd = pwd + specChar; 
-            break;
-        case numCharPos: 
-            pwd = pwd + numChar.toString (); 
-            break;
+        // pick a random character type using the array 
+        randomType = GetRandomNumInRange (0, charTypeList.length - 1); 
+        //console.log ("cell " + randomType + " value " + charTypeList[randomType]); 
 
-        /********************************************
-         * what do we want in our randomized positions? How 
-         * about numbers and characters only? 
-         *******************************************/
-        default: 
+        switch (charTypeList[randomType]) {
+        case "U":  /* upper case */
+            randomChar = GetChar (GetRandomNumInRange (65, 90));
+            break;
+        case "L": /* lower case */
+            randomChar = GetChar (GetRandomNumInRange (97, 122));
+            break;
+        case "S": /*special characters */
+            randomChar = GetChar (GetRandomNumInRange (35, 47));
+            break;
+        case "N": /* numeric */ 
+            randomChar = GetRandomNumInRange (0, 9).toString();
+            break; 
+        default: /* ERROR */
+            randomChar = "X";
+            break; 
+        } /* switch randomType */
 
-            randomType = GetRandomNumInRange (1, 3); 
-            switch (randomType) {
-            case 1:  /* upper case */
-                randomChar = GetChar (GetRandomNumInRange (65, 90));
-                break;
-            case 2: /* lower case */
-                randomChar = GetChar (GetRandomNumInRange (97, 122));
-                break;
-            case 3: /* numeric */ 
-                randomChar = GetRandomNumInRange (0, 9).toString();
-                break; 
-            } /* switch randomType */
-            pwd = pwd + randomChar;
+        pwd = pwd + randomChar;
             
-        } /* switch i */  
     } /* for loop i */ 
+
     console.log (pwd); 
-}
+
+} // GeneratePwd
+
+/************************************************
+ * called from the button. Do the edits, then Generate()
+/***********************************************/
 
 function Generate () {
+
     document.getElementById('pwdText').value = '';
+    pwdLength = document.getElementById('pwdLength').value;
+
+    // ******************************************
+    // edits. first the length 
+    // ******************************************
+
+    //alert ("selected len " + pwdLength); 
+    if (isNaN(pwdLength) === true) { 
+        alert ("ERROR! You must enter a length between " + pwdMinLength + " and " + pwdMaxLength);
+        return;
+    }
+    if (pwdLength < pwdMinLength) {
+        alert ("ERROR! Password length must exceed " + (pwdMinLength - 1) + " characters");
+        return;
+    }
+    if (pwdLength > pwdMaxLength) {
+        alert ("ERROR! Password length must not exceed " + pwdMaxLength + " characters");
+        return;
+    }
+
+    // ******************************************
+    // edits. now the checkboxes 
+    // ******************************************
+    //var cbx_uc=document.getElementById("cbx_uc");
+    //console.log(cbx_uc.checked); 
+
+    upperCaseSelected = document.getElementById("cbx_uc").checked;
+    lowerCaseSelected = document.getElementById("cbx_lc").checked;
+    specCharSelected = document.getElementById("cbx_sc").checked;
+    numCharSelected = document.getElementById("cbx_nc").checked;
+
+    if (upperCaseSelected === false && lowerCaseSelected === false) {
+        alert ("ERROR! You must select at least one of upper or lower case characters");
+        return;
+    }
+
+    // ******************************************
+    // all good. generate 
+    // ******************************************
+
     GeneratePwd();
     document.getElementById('pwdText').value = pwd; 
-}
+
+} // Generate
+
+/************************************************
+ * copy to clipboard
+/***********************************************/
+
 function CopyToClipboard () {
+
     var ta = document.querySelector ("#pwdText"); 
     ta.select();
     document.execCommand("copy"); 
     alert("copy complete"); 
-}
+
+} // CopyToClipboard
 
 /* **********************************************
 *************************************************/
